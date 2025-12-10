ab80dfcd1959cc04fa70997a6c7a73ed307b9cdf
Emma S <eschelhase@gmail.com>
2025-12-10 23:11:44 +0200

    Revise comments and add package loading in analysis script
    Updated comments for clarity and added package loading instructions.

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,6 @@
 ## run_analysis.R
-## Load the following packages:
 
-## >library(dplyr)
-## >library(data.table)
-
-## Download the dataset:
+## How to download the dataset:
 ## NB: The code should have a file run_analysis.R in the main directory that can be run
 ## as long as the Samsung data is in your working directory.
 
@@ -14,3 +10,21 @@
 ## > unzip(filename)
 
 run_analysis <- function() {
+
+## Load the following packages
+
+>library(dplyr)
+>library(data.table)
+
+## Reading & formatting the data and metadata:
+
+> feature_names <- read.table("UCI HAR Dataset/features.txt")
+> activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
+> subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+> feature_train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+> activity_train <- read.table("UCI HAR Dataset/train/Y_train.txt", header = FALSE)
+> subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+> feature_test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+> activity_test <- read.table("UCI HAR Dataset/test/Y_test.txt", header = FALSE)
+
+## rf
