48b728ff94ad9471b188a75a5ed8c0139d8d21c8
Emma S <eschelhase@gmail.com>
2025-12-10 23:07:22 +0200

    Load packages and download Samsung dataset
    Added code to load necessary packages and download the dataset.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,15 @@
 ## run_analysis.R
+## Load the following packages:
+
+>library(dplyr)
+>library(data.table)
+
+## Download the dataset:
+## NB: The code should have a file run_analysis.R in the main directory that can be run
+## as long as the Samsung data is in your working directory.
+
+> fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+> filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
+> download.file(fileURL, filename)
+> unzip(filename)
+
